42bc57a67f35d6b344bfcd4e95d212be48e801eb
Stephen Knupfer <stephenrknupfer@hotmail.com>
2021-11-21 13:16:20 -0300

    post props

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from 'react'
 import {
   SafeAreaView,
   StatusBar,
-} from 'react-native';
-import HomeScreen from './src/screens/Home';
-import Post from './src/components/Post';
+} from 'react-native'
+import HomeScreen from './src/screens/Home'
+import Post from './src/components/Post'
+
+import feed from './assets/data/feed'
+
+const post1 = feed[0]
+const post2 = feed[1]
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <StatusBar barStyle={'light-content'} />
-      <Post />
+      <Post post={post1} />
+      <Post post={post2} />
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default App;
+export default App
